c2220cdec8197cd80b293cc33a0d453691c85654
Ahmed Tahami <ahmedtahami@outlook.com>
2021-08-10 17:45:07 +0500

    Inital Commit

---
+++ AgencyBizBook/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AgencyBizBook.Startup))]
+namespace AgencyBizBook
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
